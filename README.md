0ecf0ab28c4354483eb82ee3da3a9c0038bb7c13
Jade Geels <jade@justbetter.nl>
2024-10-30 13:04:30 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# laravel-translation-checker
+# Laravel translation checker
+
+This GitHub action can be used to check if all translations are contained within the given translation files.
+
+This works by scanning through all the blade files under the `resources/` folder and comparing all found translation strings to the existing json files.
+
+## Usage
+
+To add this action into your GitHub workflow:
+
+```yaml
+- name: Check translations
+  uses: rapidez/laravel-translation-checker@1.0.0
+```
+
+It's also useful to install your dependencies beforehand, in case you need the translations from those to work:
+
+```yaml
+- name: Install dependencies
+  run: composer install --prefer-dist --no-interaction
+```
+
+## Example workflow
+
+```yaml
+name: Check translations
+
+on:
+  push:
+    branches:
+      - master
+      - '*.x'
+  pull_request:
+
+jobs:
+  translations:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v2
+      - name: Install dependencies
+        run: composer install --prefer-dist --no-interaction
+      - name: Check translations
+        uses: rapidez/laravel-translation-checker@1.0.0
+```
---
+++ check-translations.php
@@ -0,0 +1,102 @@
+<?php
+
+// Adapted from: https://github.com/barryvdh/laravel-translation-manager/blob/master/src/Manager.php
+$translationKeys = [];
+$functions = ['@lang', '__'];
+$stringPattern =
+    "[^\w]" .                                       // Must not have an alphanum before real method
+    '(' . implode('|', $functions) . ')' .          // Must start with one of the functions
+    "\(\s*" .                                       // Match opening parenthesis
+    "(?P<quote>['\"])" .                            // Match " or ' and store in {quote}
+    "(?P<string>(?:\\\k{quote}|(?!\k{quote}).)*)" . // Match any string that can be {quote} escaped
+    "\k{quote}" .                                   // Match " or ' previously matched
+    "\s*[\),]";                                     // Close parentheses or new parameter
+
+$labelPattern =
+    "label=" .                                      // Match `label=`
+    "(?P<quote>['\"])" .                            // Match " or ' and store in {quote}
+    "(?P<string>(?:\\\k{quote}|(?!\k{quote}).)*)" . // Match any string that can be {quote} escaped
+    "\k{quote}";                                    // Match " or ' previously matched
+
+$files = [];
+$iterator = new RecursiveDirectoryIterator('resources');
+foreach (new RecursiveIteratorIterator($iterator) as $file) {
+    if (strpos($file, '.blade.php') !== false) {
+        $files[] = $file->getRealPath();
+    }
+}
+
+foreach ($files as $file) {
+    $contents = file_get_contents($file);
+    if (preg_match_all("/{$stringPattern}/siU", $contents, $matches)) {
+        foreach ($matches['string'] as $key) {
+            $translationKeys[] = $key;
+        }
+    }
+    if (preg_match_all("/{$labelPattern}/siU", $contents, $matches)) {
+        foreach ($matches['string'] as $key) {
+            if (
+                str_starts_with($key, '{{')
+                || str_starts_with($key, '@lang(')
+                || str_starts_with($key, '__(')
+                || $key == 'false'
+                || ! $key
+            ) {
+                continue;
+            }
+            $translationKeys[] = $key;
+        }
+    }
+}
+
+$languages = glob('lang/*.json');
+
+if (! $languages) {
+    error_log('No language files found.');
+    exit(2);
+}
+
+$missing = [];
+foreach ($languages as $language) {
+    $language = basename($language, '.json');
+    $translationKeys = array_unique($translationKeys);
+
+    $translations = (array) json_decode(file_get_contents("lang/$language.json"));
+    $vendorfiles = array_merge(
+        glob("vendor/*/*/lang/$language.json"),
+        glob("vendor/*/*/resources/lang/$language.json")
+    );
+    foreach($vendorfiles as $file) {
+        $translations = array_merge(
+            $translations,
+            (array) json_decode(file_get_contents($file))
+        );
+    }
+
+    $missing[$language] = 0;
+    foreach ($translationKeys as $key) {
+        if (! array_key_exists($key, $translations)) {
+            $missing[$language]++;
+            error_log("missing translation in language '$language': \"{$key}\"");
+        }
+    }
+}
+
+$totalMissing = 0;
+foreach($missing as $language => $missingLang) {
+    if ($missingLang > 0) {
+        if ($missingLang == 1) {
+            error_log("1 translation is missing in language '$language'.");
+        } else {
+            error_log("$missingLang translations are missing in language '$language'.");
+        }
+
+        $totalMissing += $missingLang;
+    }
+}
+
+if($totalMissing > 0) {
+    exit(1);
+}
+
+echo('No missing translations found! :)');
